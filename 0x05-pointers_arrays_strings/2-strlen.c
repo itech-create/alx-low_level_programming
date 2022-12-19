256a4e607e51baf399ff0880e258c4664479170f
Ogunleye Oluwasemilore <ogunleyesemilore2@gmail.com>
2022-12-19 05:08:22 -0800

    Task 0 - 4

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
- /**
-  * _strlen - returns the length of a string
-  *
-  * @s: string parameter input
-  *
-  * Return: length of string
-  */
+/**
+* _strlen - returns the length of a string
+*
+* @s: string parameter input
+*
+* Return: length of string
+*/
 
 int _strlen(char *s)
 {
